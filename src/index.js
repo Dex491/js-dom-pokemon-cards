819a77cdfb773bf5e0d201fc8b8f9772d3594686
wagnerAJS221 <wagneralex221@gmai.com>
2022-10-18 15:44:29 +0100

    h2 element added and classes added as well

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
+console.log('hello')
 
-console.log('hello');
-
-//You can start simple and just render a single 
-//pokemon card from the first element
+// You can start simple and just render a single
+// pokemon card from the first element
 // console.log(data[0]);
 
 // select the <ul> for placing pokemon cards
 
-
 function createAndAppendPokemonCard(pokemonJSObject) {
   const ul = document.querySelector('.cards')
   const li = document.createElement('li')
-  li.innerText('Hello')
+  li.setAttribute('class:', 'card')
+  li.innerText = 'Hello'
   ul.appendChild(li)
-  
+
   // create the HTML elements as defined in the template
+
+  const h2 = document.createElement('h2')
+  h2.setAttribute('class:', 'card--title')
+  h2.innerText = 'h2 text'
+  li.appendChild(h2)
+
   // ensure I put the right attribute values: class, src, width
   // ensure I append the right elements inside the correct ones
 
