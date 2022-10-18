9e754622594646d1a2698a3b33c55e2758589439
Dave <Daverevell491@gmail.com>
2022-10-18 15:00:47 +0100

    Prep

--- src/index.js
+++ src/index.js
@@ -4,3 +4,18 @@ console.log(data);
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+
+// select the <ul> for placing pokemon cards
+const ul = "..."
+
+function createAndAppendPokemonCard(pokemonJSObject) {
+  // create the HTML elements as defined in the template
+  // ensure I put the right attribute values: class, src, width
+  // ensure I append the right elements inside the correct ones
+
+  // once the card <li> is created and fully built up
+  // append it to the <ul>
+}
+
+// for loop, to go through each pokemon in `data`
+// and for each pokemon, call createAndAppendPokemonCard
