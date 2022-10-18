0d84cde2b3c92dc7bd52d5b7842d2eb29e5a76c2
Dave <Daverevell491@gmail.com>
2022-10-18 16:16:43 +0100

    Card created

--- src/index.js
+++ src/index.js
@@ -7,18 +7,52 @@ console.log('hello')
 // select the <ul> for placing pokemon cards
 
 function createAndAppendPokemonCard(pokemonJSObject) {
-  const ul = document.querySelector('.cards')
-  const li = document.createElement('li')
-  li.setAttribute('class:', 'card')
-  li.innerText = 'Hello'
-  ul.appendChild(li)
+  const cards = document.querySelector('.cards')
+  const card = document.createElement('li')
+  card.setAttribute('class', 'card')
+  card.setAttribute('style', 'list-style: none')
+  cards.appendChild(card)
 
   // create the HTML elements as defined in the template
 
-  const h2 = document.createElement('h2')
-  h2.setAttribute('class:', 'card--title')
-  h2.innerText = 'h2 text'
-  li.appendChild(h2)
+  const cardTitle = document.createElement('h2')
+  cardTitle.setAttribute('class', 'card--title')
+  cardTitle.innerText = 'h2 text'
+  card.appendChild(cardTitle)
+
+  const sprite = document.createElement('img')
+  sprite.setAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png')
+  card.appendChild(sprite)
+
+  const cardText = document.createElement('ul')
+  cardText.setAttribute('class', 'card--text')
+  cardText.setAttribute('style', 'list-style:none')
+  card.appendChild(cardText)
+
+  // for (let i = 0; i < 6; i++) {}
+  const HP = document.createElement('li')
+  HP.innerText = 'HP: 45'
+  cardText.appendChild(HP)
+  
+  const Att = document.createElement('li')
+  Att.innerText = 'ATTACK: 49'
+  cardText.appendChild(Att)
+
+  const Def = document.createElement('li')
+  Def.innerText = 'DEFENSE: 49'
+  cardText.appendChild(Def)
+
+  const spAtt = document.createElement('li')
+  spAtt.innerText = 'SPECIAL-ATTACK: 65'
+  cardText.appendChild(spAtt)
+
+  const spDef = document.createElement('li')
+  spDef.innerText = 'SPECIAL-DEFENSE: 65'
+  cardText.appendChild(spDef)
+
+  const Speed = document.createElement('li')
+  Speed.innerText = 'SPEED: 45'
+  cardText.appendChild(Speed)
 
   // ensure I put the right attribute values: class, src, width
   // ensure I append the right elements inside the correct ones
